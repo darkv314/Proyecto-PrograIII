a31fb5ffcd6446b74833bb8ad5cf847a66e4ddf6
Mik43la <60782219+Mik43la@users.noreply.github.com>
2020-06-21 23:46:47 -0400

    Now user only has to register once

--- app/src/main/java/com/example/appp3/FirstTimeRegister.java
+++ app/src/main/java/com/example/appp3/FirstTimeRegister.java
@@ -1,24 +1,48 @@
 package com.example.appp3;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appp3.model.User;
-import com.example.appp3.utils.Constants;
-import com.google.gson.Gson;
 
 public class FirstTimeRegister extends AppCompatActivity
 {
     private EditText setPassword;
     private EditText setNumberOfTimes;
     private Button doneButton;
+
     SqliteHelper sqliteHelper;
+
+
+
+    String firstRegister = "firstRegister";
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if (!sharedpreferences.getBoolean(firstRegister, false)) {
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean(firstRegister, Boolean.TRUE);
+            editor.apply();
+        } else {
+            Intent allSongs = new Intent(FirstTimeRegister.this, AllSongsActivity.class);
+            startActivity(allSongs);
+        }
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
